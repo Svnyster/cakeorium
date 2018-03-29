3dc7558f1c94c3d092cc545c9a87f922142d5435
Steven Theck <stevenwtheck@gmail.com>
2018-03-29 17:17:18 -0500

    Changed cakeorium recipe
    Changed cakeorium recipe from smelting a cake to crafting a cake and iron ingot together (shapeless)

--- src/main/java/com/svnyster/cakeorium/recipe/ModRecipes.java
+++ src/main/java/com/svnyster/cakeorium/recipe/ModRecipes.java
@@ -7,7 +7,8 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModRecipes {
     public static void init() {
-        GameRegistry.addSmelting(Items.CAKE, new ItemStack(ModItems.cakeoriumIngot), 0.2F);
+        //GameRegistry.addSmelting(Items.CAKE, new ItemStack(ModItems.cakeoriumIngot), 0.2F);
+        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.cakeoriumIngot), Items.IRON_INGOT, Items.CAKE);
         GameRegistry.addShapedRecipe(new ItemStack(ModItems.cakeoriumAxe), "CC", "CS", " S", 'C', ModItems.cakeoriumIngot, 'S', Items.STICK);
         GameRegistry.addShapedRecipe(new ItemStack(ModItems.cakeoriumHoe), "CC", " S", " S", 'C', ModItems.cakeoriumIngot, 'S', Items.STICK);
         GameRegistry.addShapedRecipe(new ItemStack(ModItems.cakeoriumPickaxe), "CCC", " S ", " S ", 'C', ModItems.cakeoriumIngot, 'S', Items.STICK);
